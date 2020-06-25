6229b3abc7a4874087738a3f047b48d4c0fcd607
moshikonachmias <moshiko.nachmias@gong.io>
2020-06-25 16:35:06 +0300

    code-review

--- on-send-validation.js
+++ on-send-validation.js
@@ -1,5 +1,4 @@
 /* global Office */
-
 const webConferencingDomains = new Array(
     'zoom\\.us',
     'zoom\\.com',
@@ -17,130 +16,142 @@ const webConferencingDomains = new Array(
     'lync\\.com',
     'meetings\\.ringcentral\\.com',
     'teams\\.microsoft\\.com');
-
-const webConferencingRegexCheck = new RegExp('\\b(' + webConferencingDomains.join('|') + ')\\b');
+const webConferencingRegex = new RegExp('\\b(' + webConferencingDomains.join('|') + ')\\b');
 const linkedinDomains = new Array('linkedin\\.com$', 'linkedin\\.biz$', 'glintinc\\.com$');
-const linkedinDomainsRegexCheck = new RegExp(linkedinDomains.join("|"));
-
+const linkedinDomainsRegex = new RegExp(linkedinDomains.join('|'));
 const GONG_COORDINATOR = { NAME: 'Gong Coordinator', EMAIL: 'coordinator@gong.io' };
-
-let mailboxItem;
+const ITEM_TYPE = {
+    APPOINTMENT: 'appointment',
+}
+const LOCAL_STORAGE = {
+    KEY: 'linkedin-add-in_always_invite_gong',
+    TRUE_VALUE: 'true',
+}
+const URL = {
+    ADD_IN_DOMAIN: 'https://gong-for-linkedin.s3.amazonaws.com/',
+    SCHEDULING_DIALOG: 'validation-dialog.html'
+}
+// A Flag to indicate if there is External Attendee
 let containsExternalAttendee;
+let mailboxItem;
 
 Office.initialize = function () {
     mailboxItem = Office.context.mailbox.item;
 };
 
-function addGongCoordinator(event) {
-    mailboxItem.optionalAttendees.addAsync([{
-        displayName: GONG_COORDINATOR.NAME,
-        emailAddress: GONG_COORDINATOR.EMAIL,
-    }], { asyncContext: event });
-    event.completed({ allowEvent: true });
-}
+function itemSendHandler(event) {
+    if (mailboxItem.itemType === ITEM_TYPE.APPOINTMENT) {
 
-function confirmationAndAddCoordinator(event) {
-    const addGongAutomatically = localStorage.getItem('linkedin-add-in_always_invite_gong');
-    if (addGongAutomatically != null && addGongAutomatically === 'true') {
-        addGongCoordinator(event);
-    } else {
-        Office.context.ui.displayDialogAsync('https://gong-for-linkedin.s3.amazonaws.com/validation-dialog.html', {
-            height: 70,
-            width: 30,
-            displayInIframe: true,
-        }, function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-                console.error(asyncResult.error.message);
+        function check1RequiredAttendees(requiredAttendeesResult) {
+            if (requiredAttendeesResult.status === Office.AsyncResultStatus.Succeeded) {
+
+                containsExternalAttendee = isExternalAttendeeInvited(requiredAttendeesResult.value);
+                if (!isGongCoordinatorInvited(requiredAttendeesResult.value)) {
+                    mailboxItem.optionalAttendees.getAsync({ asyncContext: requiredAttendeesResult.asyncContext }, check2OptionalAttendees);
+                } else {
+                    sendInvite(requiredAttendeesResult.asyncContext);
+                }
             } else {
-                const dialog = asyncResult.value;
-                dialog.addEventHandler(Office.EventType.DialogMessageReceived, function (messageEvent) {
-                    dialog.close();
-                    if (messageEvent.message === 'yes') {
-                        addGongCoordinator(event);
+                console.error(requiredAttendeesResult.error);
+            }
+        }
+
+        function check2OptionalAttendees(optionalAttendeesResult) {
+            if (optionalAttendeesResult.status === Office.AsyncResultStatus.Succeeded) {
+
+                if (!isGongCoordinatorInvited(optionalAttendeesResult.value) && (containsExternalAttendee || isExternalAttendeeInvited(optionalAttendeesResult.value))) {
+                    mailboxItem.location.getAsync({ asyncContext: optionalAttendeesResult.asyncContext }, check3Location);
+                } else {
+                    sendInvite(optionalAttendeesResult.asyncContext);
+                }
+            } else {
+                console.error(optionalAttendeesResult.error);
+            }
+        }
+
+        function check3Location(locationResult) {
+            if (locationResult.status === Office.AsyncResultStatus.Succeeded) {
+                const location = locationResult.value;
+                const containsWebConfInLocation = webConferencingRegex.test(location.toLowerCase());
+
+                if (!containsWebConfInLocation) {
+                    mailboxItem.body.getAsync('html', { asyncContext: locationResult.asyncContext }, check4Body);
+                } else {
+                    confirmationAndAddCoordinator(locationResult.asyncContext);
+                }
+            } else {
+                console.error(locationResult.error);
+            }
+        }
+
+        function check4Body(bodyResult) {
+            const body = bodyResult.value;
+            const lowerCaseBody = body.toLowerCase();
+            const containsWebConf = webConferencingRegex.test(lowerCaseBody);
+
+            if (!containsWebConf) {
+                sendInvite(bodyResult.asyncContext);
+            } else {
+                confirmationAndAddCoordinator(bodyResult.asyncContext);
+            }
+        }
+
+        function confirmationAndAddCoordinator(event) {
+            const addGongAutomatically = localStorage.getItem(LOCAL_STORAGE.KEY);
+            if (addGongAutomatically != null && addGongAutomatically === LOCAL_STORAGE.TRUE_VALUE) {
+                addGongCoordinator(event);
+            } else {
+                Office.context.ui.displayDialogAsync(URL.ADD_IN_DOMAIN + URL.SCHEDULING_DIALOG, {
+                    height: 70,
+                    width: 30,
+                    displayInIframe: true,
+                }, function (asyncResult) {
+                    if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                        console.error(asyncResult.error.message);
                     } else {
-                        event.completed({ allowEvent: true });
+                        const dialog = asyncResult.value;
+                        dialog.addEventHandler(Office.EventType.DialogMessageReceived, function (messageEvent) {
+                            dialog.close();
+                            if (messageEvent.message) {
+                                addGongCoordinator(event);
+                            }
+                            sendInvite(event);
+                        });
+                        dialog.addEventHandler(Office.EventType.DialogEventReceived, function () {
+                            dialog.close();
+                            sendInvite(event);
+                        });
                     }
                 });
-                dialog.addEventHandler(Office.EventType.DialogEventReceived, function () {
-                    dialog.close();
-                    event.completed({ allowEvent: true });
-                });
             }
-        });
-    }
-}
-
-function check4Body(asyncResult) {
-    const body = asyncResult.value;
-    const lowerCaseBody = body.toLowerCase();
-    const containsWebConf = webConferencingRegexCheck.test(lowerCaseBody);
-
-    if (!containsWebConf) {
-        asyncResult.asyncContext.completed({ allowEvent: true });
-    } else {
-        confirmationAndAddCoordinator(asyncResult.asyncContext);
-    }
-}
-
-function check3Location(asyncResult) {
-    if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-        const location = asyncResult.value;
-        const containsWebConfInLocation = webConferencingRegexCheck.test(location.toLowerCase());
-
-        if (!containsWebConfInLocation) {
-            mailboxItem.body.getAsync('html', { asyncContext: asyncResult.asyncContext }, check4Body);
-        } else {
-            confirmationAndAddCoordinator(asyncResult.asyncContext);
         }
-    } else {
-        console.error(asyncResult.error);
-    }
-}
 
-function check2OptionalAttendees(asyncResult) {
-    if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-        const optionalAttendees = asyncResult.value;
-        const containsGong = optionalAttendees.some(function (email) {
-            return email.emailAddress === GONG_COORDINATOR.EMAIL
-        });
-        containsExternalAttendee = containsExternalAttendee || optionalAttendees.some(function (email) {
-            return !linkedinDomainsRegexCheck.test(email.emailAddress)
-        });
-
-        if (!containsGong && containsExternalAttendee) {
-            // check web conf in location
-            mailboxItem.location.getAsync({ asyncContext: asyncResult.asyncContext }, check3Location);
-        } else {
-            asyncResult.asyncContext.completed({ allowEvent: true });
+        function addGongCoordinator(event) {
+            mailboxItem.optionalAttendees.addAsync([{
+                displayName: GONG_COORDINATOR.NAME,
+                emailAddress: GONG_COORDINATOR.EMAIL,
+            }], { asyncContext: event });
         }
-    } else {
-        console.error(asyncResult.error);
-    }
-}
 
-function check1RequiredAttendees(asyncResult) {
-    if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-        const requiredAttendees = asyncResult.value;
-        containsExternalAttendee = requiredAttendees.some(function (email) {
-            return !linkedinDomainsRegexCheck.test(email.emailAddress)
-        });
-
-        const containsGong = requiredAttendees.some(function (email) {
-            return email.emailAddress === GONG_COORDINATOR.EMAIL
-        });
-
-        if (!containsGong) {
-            mailboxItem.optionalAttendees.getAsync({ asyncContext: asyncResult.asyncContext }, check2OptionalAttendees);
-        } else {
-            // sendEvent()
-            asyncResult.asyncContext.completed({ allowEvent: true });
+        function sendInvite(event) {
+            event.completed({ allowEvent: true });
         }
-    } else {
-        console.error(asyncResult.error);
-    }
-}
 
-function itemSendHandler(event) {
-    mailboxItem.requiredAttendees.getAsync({ asyncContext: event }, check1RequiredAttendees);
-    // add all function here
+        function isExternalAttendeeInvited(attendees) {
+            return attendees.some(function (attendee) {
+                return !linkedinDomainsRegex.test(attendee.emailAddress)
+            });
+        }
+
+        function isGongCoordinatorInvited(attendees) {
+            return attendees.some(function (attendee) {
+                return attendee.emailAddress === GONG_COORDINATOR.EMAIL
+            });
+        }
+
+        mailboxItem.requiredAttendees.getAsync({ asyncContext: event }, check1RequiredAttendees);
+    } else {
+        event.completed({ allowEvent: true });
+    }
+
 }
